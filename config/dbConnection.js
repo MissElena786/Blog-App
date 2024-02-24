fa7c2b583cf23014d13988646ed158306d423f9e
MissElena786 <bgulnaz009@gmail.com>
2024-02-24 14:32:54 +0530

    blog app

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 mongoose.set('strictQuery', false)
 
+// const uri = 'mongodb://localhost:27017/your-database-name';
+const uri = 'mongodb://127.0.0.1:27017/BlogDB';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connectionToDB = async ()=>{
    try {
       const { connection } = await mongoose.connect(
